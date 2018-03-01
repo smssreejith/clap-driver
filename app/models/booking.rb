f37c6bbc626eb7deaeba8ca4da7b1b24abe656c1
sreejithgp <sreejith.gp@mobme.in>
2018-03-01 12:33:19 +0530

    settlement issue fix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -32,43 +32,41 @@ class Booking < ApplicationRecord
       self.agency_share = share
       self.driver_payment = share
       self.clap_share = self.after_expense_trip - share
+    end
 
-
-      if driver.plan == "Daily"
-        ride_start = Date.parse(self.ride_start).strftime("%Y%m%d").to_i
-        ride_end = Date.parse(self.ride_end).strftime("%Y%m%d").to_i
-        daily_amount = (ride_end - ride_start + 1) * driver.daily_charge
-        DriverSettlement.create(booking_id: 0, driver_id: self.driver_id, amount: daily_amount)
-      elsif driver.plan == "Monthly"
-        unless DriverSettlement.where(booking_id: 0).last.created_at.strftime("%Y%m") == Time.now.strftime("%Y%m")
-          DriverSettlement.create(booking_id: 0, driver_id: self.driver_id, amount: driver.monthly_charge)
-        end
+    if driver.plan == "Daily"
+      ride_start = self.ride_start.strftime("%Y%m%d").to_i
+      ride_end = self.ride_end.strftime("%Y%m%d").to_i
+      daily_amount = (ride_end - ride_start + 1) * driver.daily_charge
+      DriverSettlement.create(booking_id: 0, driver_id: self.driver_id, amount: daily_amount)
+    elsif driver.plan == "Monthly"
+      unless DriverSettlement.where(booking_id: 0).last.created_at.strftime("%Y%m") == Time.now.strftime("%Y%m")
+        DriverSettlement.create(booking_id: 0, driver_id: self.driver_id, amount: driver.monthly_charge)
       end
+    end
 
-      customer_pending_amount = self.net_billing.to_i - self.collected_from_customer.to_i
-      driver_settlement = DriverSettlement.find_by_booking_id(self.id)
-      customer_settlement = CustomerSettlement.find_by_booking_id(self.id)
-      if ["Driver", "Agent"].include? self.payment_collected_by
-        amount = self.clap_share - customer_pending_amount
-      else
-        amount = self.clap_share - self.net_billing
-      end
-      if driver_settlement.blank?
-        DriverSettlement.create(booking_id: self.id, driver_id: self.driver_id, amount: amount)
-        byebug
-        CustomerSettlement.create(booking_id: self.id, customer_id: self.customer_id, amount: customer_pending_amount)
-        driver.update_attributes(amount_pending: (driver.amount_pending.to_i + amount))
-        customer.update_attributes(amount_pending: (customer.amount_pending.to_i + customer_pending_amount))
-      else
-        previous_amount_driver = driver_settlement.amount
-        previous_amount_customer = customer_settlement.amount
+    customer_pending_amount = self.net_billing.to_i - self.collected_from_customer.to_i
+    driver_settlement = DriverSettlement.find_by_booking_id(self.id)
+    customer_settlement = CustomerSettlement.find_by_booking_id(self.id)
+    if ["Driver", "Agent"].include? self.payment_collected_by
+      amount = self.clap_share - customer_pending_amount
+    else
+      amount = self.clap_share - self.net_billing
+    end
+    if driver_settlement.blank?
+      DriverSettlement.create(booking_id: self.id, driver_id: self.driver_id, amount: amount)
+      CustomerSettlement.create(booking_id: self.id, customer_id: self.customer_id, amount: customer_pending_amount)
+      driver.update_attributes(amount_pending: (driver.amount_pending.to_i + amount))
+      customer.update_attributes(amount_pending: (customer.amount_pending.to_i + customer_pending_amount))
+    else
+      previous_amount_driver = driver_settlement.amount
+      previous_amount_customer = customer_settlement.amount
 
-        driver_settlement.update_attributes(booking_id: self.id, driver_id: self.driver_id, amount: amount)
-        customer_settlement.update_attributes(booking_id: self.id, customer_id: self.customer_id, amount: customer_pending_amount)
+      driver_settlement.update_attributes(booking_id: self.id, driver_id: self.driver_id, amount: amount)
+      customer_settlement.update_attributes(booking_id: self.id, customer_id: self.customer_id, amount: customer_pending_amount)
 
-        driver.update_attributes(amount_pending: (driver.amount_pending.to_i + amount - previous_amount_driver))
-        customer.update_attributes(amount_pending: (customer.amount_pending.to_i + customer_pending_amount - previous_amount_customer))
-      end
+      driver.update_attributes(amount_pending: (driver.amount_pending.to_i + amount - previous_amount_driver))
+      customer.update_attributes(amount_pending: (customer.amount_pending.to_i + customer_pending_amount - previous_amount_customer))
     end
   end
 end
